f58018f50fa149a1ef3bbbde0bbbeb52994ede3c
bokun-cortlandcredit <79589449+bokun-cortlandcredit@users.noreply.github.com>
2023-12-08 11:17:58 -0500

    Add files via upload

--- Keep Me Up.py
+++ Keep Me Up.py
@@ -6,42 +6,25 @@ Created on Fri Jun 30 07:37:14 2023
 """
 
 import time, pyautogui
-import PySimpleGUI as sg
 import multiprocessing
 from datetime import datetime
 import random
-# global ddl,p1,p2
 
 def KeepUI():
-    
 
-    # time.sleep(3)
-    # window.close()
-    
     p2 = multiprocessing.Process(target = dontsleep)
     p2.start()
     
     while True:
 
-        if datetime.now()>=datetime(2023,7,5,12,4): # if user closes window or clicks cancel
+        if datetime.now()>=datetime(2023,12,8,12,4): # if user closes window or clicks cancel
 
             if p2.is_alive(): 
                 p2.terminate()
             break
-        
-        # elif datetime.now()>=datetime(2023,6,30,8,37):
-        #     window.close()
-        
-        # elif datetime.now()>=ddl:
-            
-        #     if p2.is_alive(): 
-        #         p2.terminate()
-        #     break
-            
-            
+
 
 def dontsleep():
-    # print(test)
     while True:
         pyautogui.press('volumedown')
         time.sleep(1)
@@ -56,16 +39,8 @@ def dontsleep():
         pyautogui.moveRel(-hor, -ver)
         time.sleep(5)
         
-        # if datetime.now()>=datetime(2023,6,30,8,31):
-        #     window.close()
-
 
 if __name__ == '__main__':
-    # ddl=datetime(2023,6,30,8,8)
+
     p1 = multiprocessing.Process(target = KeepUI)
     p1.start()
-    
-    # while datetime.now()<ddl:
-    #     continue
-    
-    # p1.terminate()
